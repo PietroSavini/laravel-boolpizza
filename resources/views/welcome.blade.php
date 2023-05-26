29a154bc20b4f70ec43c60e15989ef0318bd1566
simolorevg <simoneviaggi@libero.it>
2023-05-26 17:29:44 +0200

    add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,48 +17,52 @@
 
 <body>
 
-@extends('layouts.app')
+    @extends('layouts.app')
 
-@section('content')
-    <h1 class="text-center py-5">I nostre pizze</h1>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Pizza</th>
-                            <th scope="col">Ingredienti</th>
-                            <th scope="col">Base bianca</th>
-                            <th scope="col">Prezzo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($pizzas as $pizza)
+    @section('content')
+        <div class="container d-flex justify-content-center p-2" height="200">
+            <img src="{{ Vite::asset('resources\img\boolpizza_logo.svg') }}" alt="Yummy pizza!!!!" width="200">
+        </div>
+        <h1 class="text-center py-5">I nostre pizze</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <table class="table">
+                        <thead>
                             <tr>
-                                <th>{{ $pizza->nome }}</th>
-                                <td>{{ $pizza->ingredienti }}</td>
-                                <td>@if($pizza->base_bianca)
-                                        @php
-                                            $pizza->base_bianca ="Bianca";
-                                        @endphp
-                                    @else 
-                                        @php
-                                            $pizza->base_bianca="Rossa";
-                                        @endphp
-                                    @endif
-                                    {{ $pizza->base_bianca }}
-                                </td>
-                                <td>{{ $pizza->prezzo }}€</td>
+                                <th scope="col">Pizza</th>
+                                <th scope="col">Ingredienti</th>
+                                <th scope="col">Base bianca</th>
+                                <th scope="col">Prezzo</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($pizzas as $pizza)
+                                <tr>
+                                    <th>{{ $pizza->nome }}</th>
+                                    <td>{{ $pizza->ingredienti }}</td>
+                                    <td>
+                                        @if ($pizza->base_bianca)
+                                            @php
+                                                $pizza->base_bianca = 'Bianca';
+                                            @endphp
+                                        @else
+                                            @php
+                                                $pizza->base_bianca = 'Rossa';
+                                            @endphp
+                                        @endif
+                                        {{ $pizza->base_bianca }}
+                                    </td>
+                                    <td>{{ $pizza->prezzo }}€</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-@endsection
-    
+    @endsection
+
 
 </body>
 
